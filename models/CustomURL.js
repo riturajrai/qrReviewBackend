c280b425ada88364d1f7b9f10ad2d27504b7efd5
Your Name <you@example.com>
2026-01-28 12:10:25 +0530

    new updated code

--- models/CustomURL.js
+++ models/CustomURL.js
@@ -8,10 +8,24 @@ const customURLSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+
+  companyName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
   url: {
     type: String,
     required: true,
   },
+  redirectFromRating: {
+    type: Number,
+    default: 3,
+    min: 1,
+    max: 5
+  },
+
   createdAt: {
     type: Date,
     default: Date.now,
