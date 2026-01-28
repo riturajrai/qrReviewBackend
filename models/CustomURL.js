2b0e72e3df344b4ba4b97e6b6398685dfec7f20a
Your Name <you@example.com>
2026-01-28 12:43:30 +0530

    new updated code

--- models/CustomURL.js
+++ models/CustomURL.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // models/CustomURL.js
 import mongoose from "mongoose";
 
@@ -35,41 +35,4 @@ const customURLSchema = new mongoose.Schema({
 
 const CustomURL = mongoose.model("CustomURL", customURLSchema);
 export default CustomURL;
-=======
-// models/CustomURL.js
-import mongoose from "mongoose";
 
-const customURLSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-    unique: true,
-  },
-
-  companyName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-
-  url: {
-    type: String,
-    required: true,
-  },
-  redirectFromRating: {
-    type: Number,
-    default: 3,
-    min: 1,
-    max: 5
-  },
-
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-const CustomURL = mongoose.model("CustomURL", customURLSchema);
-export default CustomURL;
->>>>>>> 7e25b21c0e27f87d3e4fa7546fd88f7d6f0ef528
